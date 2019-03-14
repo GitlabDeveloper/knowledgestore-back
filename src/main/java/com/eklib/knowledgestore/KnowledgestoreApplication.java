22ec4e171d471797faf41be7605789debd6a8a04
AlexeyRadkevich <arr.scout@gmail.com>
2019-03-14 11:37:49 +0300

    Added basic spring boot application structure

---
+++ src/main/java/com/eklib/knowledgestore/KnowledgestoreApplication.java
@@ -0,0 +1,13 @@
+package com.eklib.knowledgestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eklib/knowledgestore/KnowledgestoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eklib.knowledgestore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KnowledgestoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
